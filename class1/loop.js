82c5ac01703c751727ca3c2c17d9b9492c95a746
MD:MAZEDUL ISLAM <125912440+mazed2020@users.noreply.github.com>
2024-07-09 08:29:09 +0000

    adding loop file in folder

---
+++ class1/loop.js
@@ -0,0 +1,75 @@
+
+// initial value check only one time after that check condition repeatedly after that increment value more more
+// for(let i=0; i<10;  i++){
+//     console.log(`outer loop ${i}`)
+//     for(let j=0; j<10; j++){
+//         console.log(`inner loop value print ${j}`)
+//     }
+// }
+
+// let i=0;
+// while(i<10){
+//     console.log( `print just its value ${i}`);
+//     i++;
+// }
+
+//  loop for (for of),for in ;
+
+
+// for of loop in javascript
+// const arr=[1,2,4,5,5];
+// for(let value of arr){
+//     console.log(value);
+// }
+// const map=new Map();
+// map.set('mazed',23);
+// map.set('mahfuz',24);
+// map.set('masaid',34);
+// console.log(map)
+// for(let name of map){
+//     console.log(name[1]);
+// }
+
+// const obj={
+//     name:'mazed',
+//     age:23,
+//     village:"baliyapara"
+// }
+// for(let value of obj){
+//     console.log(value);
+// }
+/*============
+for of loop work:
+array,map,
+it not work in object;
+*/
+// for of loop in javascript
+// const arr=[1,2,4,5,5];
+// for(let value in arr){
+//     console.log(value, arr[value]);
+// }
+
+
+// const map=new Map();
+// map.set('mazed',23);
+// map.set('mahfuz',24);
+// map.set('masaid',34);
+// // console.log(map)
+// for(let name in map){
+//     console.log(name);
+// }
+
+// const obj={
+//     name:'mazed',
+//     age:23,
+//     village:"baliyapara"
+// }
+// for(let key in obj){
+//     console.log(key,obj[key]);
+// }
+
+/*============
+for in loop work:
+array,obj,
+it not work in map data structure in javascipt;
+*/
