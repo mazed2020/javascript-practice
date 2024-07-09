ca0a5751c73491a3ff68005d19cbe4f5bf6516e4
MD:MAZEDUL ISLAM <125912440+mazed2020@users.noreply.github.com>
2024-07-09 08:09:48 +0000

    add if else file folcy value

---
+++ class1/Folcy.js
@@ -0,0 +1,35 @@
+// const username="false";
+// if(username){
+//     console.log("username found");
+// }else{
+//     console.log("username not found");
+// }
+
+//here is folcy value in javascript
+// false,"",undefince,null,0,
+//if diye code ar kisu part ke scope banano jay jekhane outsite asscess allowed nai
+
+// const age=18;
+// if(age==='18'){
+//     console.log("age 18 found");
+// }else{
+//     console.log("age not found");
+//}
+// operator ==(not check any time in javascipt),===(check also type in javascript),// <, >, <=, >=, ==, !=, ===, !==
+//+++++++++++++++++++++++++++++++++++++
+// Nullish Coalescing Operator (??): null undefined
+// const val=15 ?? 20;
+// console.log(val)
+
+// const val1=null ?? 20;
+// console.log(val1);
+
+// const val1=undefined?? 20;
+// console.log(val1);
+
+/*===========================
+short form of if else
+condition ?true:false;
+*/
+age =17;
+age>=18?console.log("He/She capable for voting"):console.log("He/she not capable for voting");
